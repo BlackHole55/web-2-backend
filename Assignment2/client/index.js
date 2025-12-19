08901d7103d6d0c5ef97c96545c1afc73f2fb284
Meirhan Syzdykov <syzdykovmr@gmail.com>
2025-12-20 01:24:11 +0500

    feat: Add Assignment 2

---
+++ Assignment2/client/index.js
@@ -0,0 +1,142 @@
+const userInfo = document.getElementById('userInfo')
+const countryInfo = document.getElementById('countryInfo')
+const currencyInfo = document.getElementById('exchangeRate')
+const newsInfo = document.getElementById('newsInfo')
+
+const userElements = {
+  profilePicture: userInfo.querySelector('#profilePicture'),
+  userName: userInfo.querySelector('#userName'),
+  gender: userInfo.querySelector('#gender'),
+  age: userInfo.querySelector('#age'),
+  dateOfBirth: userInfo.querySelector('#dateOfBirth'),
+  city: userInfo.querySelector('#city'),
+  country: userInfo.querySelector('#country'),
+  address: userInfo.querySelector('#address')
+}
+
+const countryElements = {
+    flag: countryInfo.querySelector('#flag'),
+    name: countryInfo.querySelector('#name'),
+    capital: countryInfo.querySelector('#capital'),
+    currency: countryInfo.querySelector('#currency'),
+    languages: countryInfo.querySelector('#languages')
+}
+
+const currencyElements = {
+    USD: currencyInfo.querySelector('#USD'),
+    KZT: currencyInfo.querySelector('#KZT')
+}
+
+const newsElements = {
+    headline: newsInfo.querySelector('#headline'),
+    newsImg: newsInfo.querySelector('#newsImg'),
+    description: newsInfo.querySelector('#description'),
+    source: newsInfo.querySelector('#source')
+}
+
+document.getElementById('getRandomUser').addEventListener('click', async (e) => {
+    try {
+        const responseUser = await fetch('http://localhost:3000/randomuser')
+
+        if (!responseUser.ok) {
+            throw new Error(`HTTP error (randomuser API) ${responseUser.status}`)
+        }
+
+        const dataUser = await responseUser.json()
+        const randomUser = dataUser.results[0]
+
+        const fullName = randomUser.name.first + ' ' + randomUser.name.last
+        const dob = new Date(randomUser.dob.date)
+        const age = "Age: " + randomUser.dob.age
+        const address = randomUser.location.street.name + ' ' + randomUser.location.street.number
+
+        userInfo.style.display = 'block'
+
+        userElements.profilePicture.src = randomUser.picture.large
+        userElements.userName.innerText = fullName
+        userElements.gender.innerText = randomUser.gender
+        userElements.age.innerText = age
+        userElements.dateOfBirth.innerText = new Intl.DateTimeFormat('en-GB').format(dob)
+        userElements.country.innerText = randomUser.location.country + ','
+        userElements.city.innerText = randomUser.location.city + ','
+        userElements.address.innerText = address
+
+        const responseCountry = await fetch(`http://localhost:3000/country?country=${randomUser.location.country}`)
+
+        if (!responseCountry.ok) {
+            throw new Error(`HTTP error (restcountries API) ${responseCountry.status}`)
+        }
+
+        const dataCountry = await responseCountry.json()
+        let country = ''
+        for (const value of dataCountry) {
+            if (randomUser.location.country == value.name.common) {
+                country = value
+            }
+        }
+
+        let languages = ""
+        const langCount = Object.keys(country.languages).length
+        let i = 1
+
+        for (const key in country.languages) {
+            languages += `${country.languages[key]}`
+            
+            if (i != langCount) {
+                languages += ', '
+            }
+            i += 1
+        }
+
+        const currency = Object.values(country.currencies)[0].name
+        const currencyCode = Object.keys(country.currencies)[0]
+        const countryCca2 = country.cca2.toLowerCase()
+
+        countryInfo.style.display = 'block'
+
+        countryElements.flag.src = country.flags.png
+        countryElements.name.innerText = country.name.common
+        countryElements.capital.innerHTML = country.capital
+        countryElements.languages.innerHTML = languages
+        countryElements.currency.innerText = 'Currency: ' + currency
+
+        const responseCurrency = await fetch(`http://localhost:3000/currency?currency=${currencyCode}`)
+
+        if (!responseCurrency.ok) {
+            throw new Error(`HTTP error (exchangerate API) ${responseCurrency.status}`)
+        }
+
+        const dataCurrency = await responseCurrency.json()
+        
+        const USD = `1 ${dataCurrency.base_code} = ${dataCurrency.conversion_rates.USD.toFixed(2)} USD`
+        const KZT = `1 ${dataCurrency.base_code} = ${dataCurrency.conversion_rates.KZT.toFixed(2)} KZT`
+        
+        currencyElements.USD.innerText = USD
+        currencyElements.KZT.innerText = KZT
+
+        const responseNews = await fetch(`http://localhost:3000/news?country=${countryCca2}`)
+
+        if (!responseNews.ok) {
+            throw new Error(`HTTP error (newsapi API) ${responseNews.status}`)
+        }
+
+        const dataNews = await responseNews.json()
+
+        newsInfo.style.display = 'block'
+
+        if (dataNews.articles.length > 0) {
+            const news = dataNews.articles[0]
+
+            newsElements.headline.innerText = news.title
+            newsElements.description.innerText = news.description
+            newsElements.newsImg.src = news.urlToImage
+            newsElements.source.src = news.url
+        }else {
+            newsElements.headline.innerText = `No news from ${country.name.common}`
+        }
+        
+
+    } catch (err) {
+        console.error("Fetch error:", err)
+    }
+});
---
+++ Assignment2/server.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) 
+
+app.use(express.static(path.join(__dirname, 'client')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/index.html'))
+})
+
+app.get('/randomuser', async (req, res) => {
+    const response = await fetch('https://randomuser.me/api/')
+    const data = await response.json()
+    res.send(data)
+})
+
+app.get('/country', async (req, res) => {
+    const country = req.query.country
+    
+    const response = await fetch(`https://restcountries.com/v3.1/name/${country}`)
+    const data = await response.json()
+    res.send(data)
+})
+
+app.get('/currency', async (req, res) => {
+    const currency = req.query.currency
+
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.EXCHANGE_RATE_API_KEY}/latest/${currency}`)
+    const data = await response.json()
+    res.send(data)
+})
+
+app.get('/news', async (req, res) => {
+    const country = req.query.country
+
+    const response = await fetch(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}`)
+    const data = await response.json()
+    res.send(data)
+})
+
+app.listen(port, () => {
+    console.log("Server running...");
+});
