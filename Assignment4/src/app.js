0664f4861066d45247fc5e50ddbce5781fe7e29d
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-01-21 12:25:26 +0500

    feat: serve index.html

--- Assignment4/src/app.js
+++ Assignment4/src/app.js
@@ -10,8 +10,7 @@ app.use(express.static(path.join(__dirname, "..", "client")));
 app.use("/climate", climateRoutes);
 
 app.get("/", (req, res) => {
-    res.send("App running");
-    // res.sendFile(path.join(__dirname, "..", "/client/index.html"));
+    res.sendFile(path.join(__dirname, "..", "/client/index.html"));
 });
 
 module.exports = app;
